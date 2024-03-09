e395da6c5446ccce2657b4e0945e3a13db506d3c
Protatooo <henrich.boom@gmail.com>
2024-03-09 12:02:09 +0800

    daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,27 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+          "primary": "#00a5ff",        
+          "secondary": "#aabece",       
+          "accent": "#dd3e46",         
+          "neutral": "#001e25",         
+          "base-100": "#003d4b",        
+          "info": "#10a7ff",         
+          "success": "#00f871",       
+          "warning": "#ffd782",        
+          "error": "#ff4646",
+          },
+        },
+      ],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
