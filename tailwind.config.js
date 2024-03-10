bf084acb734b346c655381026a4be7057967ca04
Protatooo <henrich.boom@gmail.com>
2024-03-10 17:26:47 +0800

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,30 +9,26 @@ export default {
     daisyui: {
       themes: [
         {
-          mytheme: {
-          
- "primary": "#307CBF",
-          
- "secondary": "#0600ff",
-          
- "accent": "#D77C45",
-          
- "neutral": "#232630",
-          
- "base-100": "#F2F2F2",
-          
- "info": "#007ae8",
-          
- "success": "#99C67F",
-          
- "warning": "#F2CA7E",
-          
- "error": "#D9414E",
+          mytheme: {        
+          "primary": "#307CBF",         
+          "secondary": "#0600ff",        
+          "accent": "#D77C45",         
+          "neutral": "#232630",         
+          "base-100": "#F2F2F2",          
+          "info": "#007ae8",          
+          "success": "#99C67F",          
+          "warning": "#F2CA7E",         
+          "error": "#D9414E",
           },
         },
       ],
     },
-    extend: {},
+    extend: {
+      // Pabago na lang nito
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require("daisyui")
