5cd7168d14721fb996f5eacb40fd6cec448e8375
ELI <Eliseo.lim12@gmail.com>
2024-03-31 19:42:47 +0800

    relationshep

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,11 @@ class User extends Authenticatable
         return $this->hasMany(Review::class);
     }
 
+    public function carts()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.
